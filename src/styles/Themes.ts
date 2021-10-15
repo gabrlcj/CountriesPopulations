8e0ce0ca712289409472028491c9f76ab7a0cdb5
Gabriel <gabrielbittencourt57@gmail.com>
2021-10-15 02:52:30 -0300

    :lipstick: styles: Added global styles and theme file separeted

---
+++ src/styles/Themes.ts
@@ -0,0 +1,31 @@
+// export const lightTheme = {
+//   background: 'hsl(0, 0%, 98%)',
+//   element: 'hsl(0, 0%, 100%)',
+//   text: 'hsl(200, 15%, 8%)',
+//   input: 'hsl(0, 0%, 52%)',
+//   shadow: 'rgba(205, 205, 205, 0.8)',
+// }
+
+// export const darkTheme = {
+//   background: 'hsl(207, 26%, 17%)',
+//   element: 'hsl(209, 23%, 22%)',
+//   text: 'hsl(0, 0%, 100%)',
+//   input: 'hsl(0, 0%, 52%)',
+//   shadow: 'rgba(0, 0, 0, 0.5)',
+// }
+
+export const lightTheme = {
+  background: '#f8f9fa',
+  element: '#e9ecef',
+  text: 'hsl(200, 15%, 8%)',
+  input: '#dee2e6',
+  shadow: 'rgba(205, 205, 205, 0.8)',
+}
+
+export const darkTheme = {
+  background: '#212529',
+  element: '#343a40',
+  text: 'hsl(0, 0%, 100%)',
+  input: '#495057',
+  shadow: 'rgba(0, 0, 0, 0.5)',
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,71 @@
+import { createGlobalStyle } from 'styled-components'
+import { Theme } from '../@types/types'
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    // Dark mode
+    --dark-mode-elements: hsl(209, 23%, 22%);
+    --dark-mode-bg: hsl(207, 26%, 17%);
+
+    // Light mode
+    --light-mode-text: hsl(200, 15%, 8%);
+    --input: hsl(0, 0%, 52%);
+    --light-mode-bg: hsl(0, 0%, 98%);
+
+    --white-color: hsl(0, 0%, 100%); 
+  }
+
+  *, *::after, *::before {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: ${({ theme }: Theme) => theme.background};
+    color: ${({ theme }: Theme) => theme.text};
+    font: 300 1rem 'Nunito Sans', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    transition: all 0.50s linear;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 800;
+  }
+
+  li, ul {
+    list-style: none;
+  }
+
+  img {
+    width: 280px;
+    height: 160px;
+  }
+
+  button  {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    cursor: not-allowed;
+  }
+
+  .filter-search-options {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  
+    padding: 1.5rem 5rem;
+    margin: 0 auto;
+  }
+`
