4e6d309de8fc30401a14e9a3e8c68c5fca82a9ba
Gabriel <gabrielbittencourt57@gmail.com>
2021-10-15 02:53:23 -0300

    :art: fix: Added support for dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import Home from './pages/Home'
+import { ThemeProvider } from 'styled-components'
+import { useDarkMode } from './hooks/useDarkMode'
+import { darkTheme, lightTheme } from './styles/Themes'
+import { GlobalStyles } from './styles/global'
+import Header from './components/Header'
+
 function App() {
-  return <div className='App'></div>
+  const { theme, toggleTheme } = useDarkMode()
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme
+
+  return (
+    <ThemeProvider theme={themeMode}>
+      <GlobalStyles />
+      <Header toggleTheme={toggleTheme} />
+      <Home />
+    </ThemeProvider>
+  )
 }
 
 export default App
